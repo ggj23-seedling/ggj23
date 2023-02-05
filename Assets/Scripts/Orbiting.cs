0e0e2d7a7358fd7967f56838616d9eee3e835b33
Mauro Vanetti <posta@maurovanetti.info>
2023-02-05 01:22:33 +0100

    Camera impact

--- Assets/Scripts/Orbiting.cs
+++ Assets/Scripts/Orbiting.cs
@@ -3,47 +3,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum CameraPhase
+{
+    falling,
+    orbiting
+}
+
 public class Orbiting : MonoBehaviour
 {
     public Transform target;
-    public float speed = 100f;
-    public float height = 20f;
+    public float fallingSpeed = 1000f;
+    public float fallingHeight = 2000f;
+    public float impactHeight = 1f;    
+    public float orbitingSpeed = 100f;        
+    public float orbitingHeight = 20f;
+
+    private float height;
+    private CameraPhase phase;
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (target == null)
+        {
+            Debug.LogError("Please set target of Orbiting Camera");
+        }
         transform.LookAt(target);
+        startingPosition = transform.position;
+        startingRotation = transform.rotation;
+        height = fallingHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (phase)
+        {
+            case CameraPhase.falling:
+                height -= fallingSpeed * Time.deltaTime;
+                break;
+            case CameraPhase.orbiting:
+                height = orbitingHeight;
+                break;
+        }
 
         Vector3 radiusVersor = (transform.position - target.position).normalized;
         transform.position = target.position + radiusVersor * height;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (height <= impactHeight)
         {
-            Debug.Log($"radiusVersor = {radiusVersor} ({target.position} -> {transform.position})");
-            Debug.Log($"transform.up = {transform.up}");
+            phase = CameraPhase.orbiting;
+            OnImpact();
+            transform.position = startingPosition;
+            transform.rotation = startingRotation;
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {            
-            transform.RotateAround(target.position, transform.right, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
+        if (phase == CameraPhase.orbiting)
         {
-            transform.RotateAround(target.position, transform.up, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.RotateAround(target.position, -transform.right, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.RotateAround(target.position, -transform.up, speed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log($"radiusVersor = {radiusVersor} ({target.position} -> {transform.position})");
+                Debug.Log($"transform.up = {transform.up}");
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.RotateAround(target.position, transform.right, orbitingSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.RotateAround(target.position, transform.up, orbitingSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.RotateAround(target.position, -transform.right, orbitingSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.RotateAround(target.position, -transform.up, orbitingSpeed * Time.deltaTime);
+            }
         }
 
+    }
+
+    void OnImpact()
+    {
+        Debug.LogWarning("OnImpact");
     }
 }
